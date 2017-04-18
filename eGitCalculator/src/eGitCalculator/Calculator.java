5fd363828d56cdcf36eefa79be8704aae1b6a380
cksrn2979 <cksrn2979@gmail.com>
2017-04-18 13:08:46 +0900

    Create Calculator project, with sum()

---
+++ eGitCalculator/src/eGitCalculator/Calculator.java
@@ -0,0 +1,12 @@
+package eGitCalculator;
+
+public class Calculator {
+	public int sum(int x, int y) {
+		return x + y;
+	}
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		System.out.println(calc.sum(10, 20));
+	}
+}
