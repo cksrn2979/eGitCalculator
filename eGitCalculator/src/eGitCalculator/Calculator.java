d37dd535bb65ca4881829abdf88ab4a6a94e5cad
cksrn2979 <cksrn2979@gmail.com>
2017-04-18 13:13:27 +0900

    add sub()

--- eGitCalculator/src/eGitCalculator/Calculator.java
+++ eGitCalculator/src/eGitCalculator/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
 		return x + y;
 	}
 
+	public int sub(int x, int y) {
+		return x - y;
+	}
+
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 		System.out.println(calc.sum(10, 20));
